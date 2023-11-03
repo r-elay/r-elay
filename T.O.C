97475a7d43372592519902d1da55d6589da380c6
Rayce Piper <117605287+r-elay@users.noreply.github.com>
2023-11-03 18:32:53 -0500

    Create T.O.C

---
+++ T.O.C
@@ -0,0 +1,122 @@
+# T.O.C
+And#
+{
+ Department of Construction Science, College of Architecture, Texas A&M University, College Station, TX, 2022.
+  "semi": true,
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": true,
+  "arrowParens": "IEEE"
+}
+Wish list - P3   October|
+Design / Industrial - P4
+To - Do - P5
+Hammer and put your head into it! - P6
+Marine Energy Project - P7
+Task Page Setup - P8
+ENDS 101 - P9
+Hump-Day - P10
+Tuesday Nov 1st, 2022 - P11   November|
+Sustainable Offshore Design - P12
+Project Methods - P13
+Model A - Intro - P14
+Material Design Flow Chart - P15
+SU2 Notes (&GitHub) - P16
+Basic Turbine - P17   December|
+Blade Layout - P17A
+Schedule - P18
+Have/Need - P19
+Wing Finder - P20
+Aggie Pitch - P21
+Spirituality - P22   Left blank intentionally
+=3 Turnaround
+Project Ctd. - P23
+Presentation - P24
+=3 Team Collaboration 100%
+AWIC Meeting - P25
+{
+"semi": true, - P26
+"trailingComma": "es5" - P27
+"singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": - P28
+true,
+  "arrowParens" - P29
+"AWIC" - P30
+}
+New Year - P31   January|
+Start Irving - P31
+Start Irving - P32
+First Week @ Miinc - Pg 34
+#First Week @ Miinc - P35
+#First Week @ Miinc - P36
+#First Week @ Miinc - P37
+PTES - P38
+#PTES - P39
+Recap - Plumbing fixtures - P40
+Bluebeam account - P41
+First week of Estimating - P42
+Week 2: NEC Guidelines - P43
+Timesheet - 1/23 thru 1/24 - P44
+#Timesheet - 1/23 thru 1/24 - P45
+And# Weekly Project Manager Meeting - P46
+#Weekly Project Manager Meeting - P47
+=3 Weekend
+Fri - Yay! - P48   February|
+#Fri - Yay!  - P49
+=3 Work
+Created P.E.P - P50
+Isolation Valves - P51
+Lifting, Launching, and Upending - P52
+Project Pike - P53
+Turbine Nomenclature - P54
+Business Model Controls - P55
+And# Landover Project Rates - P56
+Details - P57
+{
+"semi": true,
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": true,
+  "arrowParens": "Details"
+Merit Press/HMC Partnership - P58
+Scheduling - Manpower loaded - P59
+Open - Job Takeoffs - P60
+Jobsite Visit - P61
+202_ - P62
+System Placement - P63
+Unit Scales & Dimensions - P64
+}
+=3 Planning
+Resource Loading - P65   March|
+Durations, relationships, & Logic - P67
+Sequencing and Integrating - P68
+And# 
+{
+"semi": true,
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": true,
+  "arrowParens": "Meeting Minutes"
+}
+SMU - Meeting Minutes - P69
+And#
+{
+"semi": true,
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "bracketSpacing": true,
+  "arrowParens": "Fasttracking"
+}
+Shortening a Schedule - P70
+Proving a hypothesis - P71
+DPR Construction Application - P72
